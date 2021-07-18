79b754729bbf7b9903c0ae5b8ee9cdad2682c754
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2021-07-17 21:03:01 -0300

    Define most structs

--- voronoi.cpp
+++ voronoi.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <queue>
+
 using namespace std;
 
+// TODO:
+//  - degenerate case when two sites have the same y coord;
+
+struct point {
+    double x, y;
+};
+
+typedef point site;
+
+struct circle;
+
+struct arc {
+    site s;
+    circle *c;
+    arc *prev, *next;
+};
+struct circle{
+    double lowest;
+    arc *a;
+    bool valid;
+};
+
+struct compare_events {
+    bool operator()(site a, site b) {
+        if (a.y < b.y) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool operator()(circle *a, circle *b) {
+        if (a->lowest < b->lowest) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+// Event queues
+priority_queue<site, vector<site>, compare_events> sites;
+priority_queue<circle*, vector<circle*>, compare_events> circles;
+
 int main() {
-  return 0;
+    ifstream f("points.txt");
+    site p;
+    while(f >> p.x >> p.y) {
+        sites.push(p);
+    }
+    return 0;
 }
