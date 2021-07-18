90ff631c27468bcd578c679ce5336e1550dddf88
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2021-07-17 21:35:55 -0300

    Start handling events

--- voronoi.cpp
+++ voronoi.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 // TODO:
 //  - degenerate case when two sites have the same y coord;
+//  - degenerate case when circle and site have the same y coord;
 
 struct point {
     double x, y;
@@ -15,10 +16,15 @@ typedef point site;
 
 struct circle;
 
+struct seg {
+    point start, end;
+};
+
 struct arc {
     site s;
     circle *c;
     arc *prev, *next;
+    seg *l, *r;
 };
 struct circle{
     double lowest;
@@ -46,14 +52,45 @@ struct compare_events {
 };
 
 // Event queues
+// Instead of only one queue for both events, we are using
+// two, one for each "type" of event. Because of this,
+// we will only need to compare the first element of each queue.
 priority_queue<site, vector<site>, compare_events> sites;
 priority_queue<circle*, vector<circle*>, compare_events> circles;
+
+void handle_site_event() {
+    
+}
+
+void handle_circle_event() {
+    
+}
 
 int main() {
     ifstream f("points.txt");
     site p;
-    while(f >> p.x >> p.y) {
+
+    // Initialize site event queue
+    while (f >> p.x >> p.y) {
         sites.push(p);
+    }
+
+    // While queues are not empty
+    while (!sites.empty() || !circles.empty()) {
+        if (!sites.empty() && !circles.empty()) {
+            if (sites.top().y > circles.top()->lowest) {
+                handle_site_event();
+            }
+            else {
+                handle_circle_event();
+            }
+        }
+        else if (!sites.empty()) {
+            handle_site_event();
+        }
+        else {
+            handle_circle_event();
+        }
     }
     return 0;
 }
