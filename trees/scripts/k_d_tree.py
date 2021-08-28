070ecb1951610abf30a2672ca4b20e7197c281fc
Lucas Emanuel Resck Domingues <41991486+lucasresck@users.noreply.github.com>
2021-08-28 13:50:01 -0300

    Add docstrings

--- trees/scripts/k_d_tree.py
+++ trees/scripts/k_d_tree.py
@@ -2,11 +2,33 @@ from queue import PriorityQueue
 import numpy as np
 
 class BoundedPriorityQueue(PriorityQueue):
+    '''Bounded priority queue (BPQ) implementation.
+    
+    Bounded priority queue inherited from a priority queue implementation. The
+    differences are:
+    - when the queue is full and the new element can be has enough priority to
+    be inserted, we remove the less priority element;
+    - the priority is reverted, that is, we remove the element with the 
+    greatest priority, instead of the one with the least priority. This is
+    because, in our application, small number means high priority.
+
+    Args:
+        k: size of the priority queue.
+
+    Attributes:
+        max_priority: maximum priority.
+    '''
     def __init__(self, k):
         super().__init__(k)
         self.max_priority = 0
 
     def put_item(self, node,  priority):
+        '''Put the item into the bounded priority queue.
+        
+        Args:
+            node: the item to be added.
+            priority: priority of the node.
+        '''
         if not self.full():
             self.put((-priority, node))
             if priority > self.max_priority:
@@ -17,11 +39,20 @@ class BoundedPriorityQueue(PriorityQueue):
                 self.put((-priority, node))
 
 class KDTree:
+    '''k-d tree implementation.'''
     def __init__(self, k):
         self.tree = []
         self.k = k
 
     def add(self, node, dim=0, subtree=None):
+        '''Add a node to the tree.
+        
+        Args:
+            node: node to be added.
+            dim: which dim to be compared. By default, it starts with dim 0 at
+                root node and increases by one for each level of the tree.
+            subtree: optional; in which subtree to add the node. By standard,
+                it is the tree itself.'''
         if subtree is None:
             subtree = self.tree
         if not len(subtree):
